544e3127457b91d2d2aa24780ea57d8cc7972fd7
azorin <azorin@moysklad.ru>
2024-05-10 14:25:09 +0300

    Первое задание

--- src/app/app.js
+++ src/app/app.js
@@ -1,6 +1,10 @@
 import html from "./app.html";
 import './app.css'
-
+import { initFlowbite } from 'flowbite'
 
 const rootElement = document.getElementById('root');
 rootElement.innerHTML = html;
+
+rootElement.onload(() => {
+    initFlowbite();
+})
